6b1c48b9263c64f106fb1171c820837bb2b8701d
jefjvincent <43353689+jefjvincent@users.noreply.github.com>
2020-04-09 11:55:09 -0400

    Add project files.

---
+++ EventListGenerator.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace EventDataGenerator
+{
+    partial class EventListGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEventList = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEventList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEventList
+            // 
+            this.dgvEventList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEventList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.txtId,
+            this.txtName});
+            this.dgvEventList.Location = new System.Drawing.Point(12, 12);
+            this.dgvEventList.Name = "dgvEventList";
+            this.dgvEventList.Size = new System.Drawing.Size(406, 272);
+            this.dgvEventList.TabIndex = 0;
+            this.dgvEventList.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEventList_CellValueChanged);
+            this.dgvEventList.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dgvEventList_UserDeletingRow);
+            // 
+            // txtId
+            // 
+            this.txtId.DataPropertyName = "Id";
+            this.txtId.HeaderText = "Id";
+            this.txtId.Name = "txtId";
+            this.txtId.Visible = false;
+            // 
+            // txtName
+            // 
+            this.txtName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.txtName.DataPropertyName = "EventName";
+            this.txtName.HeaderText = "EventName";
+            this.txtName.Name = "txtName";
+            // 
+            // EventListGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvEventList);
+            this.Name = "EventListGenerator";
+            this.Text = "EventListGenerator";
+            this.Load += new System.EventHandler(this.EventListGenerator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEventList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEventList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtName;
+    }
+}
---
+++ EventListGenerator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventDataGenerator
+{
+    public partial class EventListGenerator : Form
+    {
+        public string constr = @"Data Source=AO-DATABASE\SQLEXPRESS;Initial catalog=ATHLETICSONTARIO;Integrated Security=False;uid=sa;Password=ao@12345";
+        public EventListGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void EventListGenerator_Load(object sender, EventArgs e)
+        {
+            PopulateDataDridView();
+        }
+        void PopulateDataDridView()
+        {
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("select * from AthleteEventListAll", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvEventList.DataSource = dt;
+            }
+        }
+
+        private void dgvEventList_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvEventList.CurrentRow != null)
+            {
+                using (SqlConnection conn = new SqlConnection(constr))
+                {
+                    try
+                    {
+                        conn.Open();
+                        DataGridViewRow dgvRow = dgvEventList.CurrentRow;
+                        SqlCommand cd = new SqlCommand("EventListAddUpdate", conn);
+                        cd.CommandType = CommandType.StoredProcedure;
+                        if (dgvRow.Cells["txtId"].Value == DBNull.Value)
+                            cd.Parameters.AddWithValue("@Id", 0);
+                        else
+                            cd.Parameters.AddWithValue("@Id", Convert.ToInt32(dgvRow.Cells["txtId"].Value));
+                        cd.Parameters.AddWithValue("@Name", dgvRow.Cells["txtName"].Value == DBNull.Value ? "" : dgvRow.Cells["txtName"].Value.ToString());
+                        cd.ExecuteNonQuery();
+                        PopulateDataDridView();
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.Message.Contains("UNIQUE"))
+                        {
+                            MessageBox.Show("Duplicate Entry", "Alert");
+                            PopulateDataDridView();
+                        }
+                        else
+                            MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void dgvEventList_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            //e.Control.KeyPress -= AllowNumbersOnly;
+            //if (dgvEventList.CurrentCell.ColumnIndex == 2)
+            //{
+            //    e.Control.KeyPress -= AllowNumbersOnly;
+            //    e.Control.KeyPress += AllowNumbersOnly;
+            //}
+
+        }
+        //private void AllowNumbersOnly(Object Sender, KeyPressEventArgs e)
+        //{
+        //    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+        //        e.Handled = true;
+        //}
+
+        private void dgvEventList_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            if (dgvEventList.CurrentRow.Cells["txtId"].Value != DBNull.Value)
+            {
+                if (MessageBox.Show("Confirm Delete", "DataGridView", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    using (SqlConnection conn = new SqlConnection(constr))
+                    {
+                        conn.Open();
+
+                        SqlCommand cd = new SqlCommand("EventListDelete", conn);
+                        
+                        cd.CommandType = CommandType.StoredProcedure;
+                        cd.Parameters.AddWithValue("@Id", Convert.ToInt32(dgvEventList.CurrentRow.Cells["txtId"].Value));
+                        cd.ExecuteNonQuery();
+                       
+                    }
+                }
+                else
+                    e.Cancel = true;
+            }
+            else
+                e.Cancel = true;
+
+
+        }
+    }
+
+}
